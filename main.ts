2446d6f2a3f876b3f98805ab9e8997d28c770aab
EdsolsInnovations <Interns@edsols.in>
2026-03-27 12:34:44 +0530

    main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ modified from chengengyue
 
 
 //% color="#0000cd" weight=20 icon="\uf002"
-namespace module_world_Color {
+namespace module_world_color {
 
     const COLOR_ADD = 0X53;
     const COLOR_REG = 0x00;
